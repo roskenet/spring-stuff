50a3a79cdc03093c7e8b158d7f3c307cbbf72731
Felix Roske <felix@roskenet.de>
2023-12-03 23:48:18 +0100

    Sync

--- part-00/src/main/java/workshop/spring/Application.java
+++ part-00/src/main/java/workshop/spring/Application.java
@@ -3,6 +3,7 @@ package workshop.spring;
 public class Application {
     public static void main( String[] args ) {
         Performer performer = new Juggler();
+
         Party app = new Party(performer);
 
         System.out.println(app.start());

52bd1ac311bde198533a628b6f0bd31cd9dfcb61
Felix Roske <felix@zalando.de>
2023-12-07 12:24:24 +0100

    Sync

--- kotlin-playground/party-application-02-annotconfig/src/main/kotlin/Main.kt
+++ kotlin-playground/party-application-02-annotconfig/src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import workshop.beans.Party
 
 fun main(args: Array<String>) {
     println("Let's have an AnnotationConfigured Party!")
+
     val ctx = AnnotationConfigApplicationContext(ApplicationConfig::class.java)
     val party = ctx.getBean(Party::class.java)
 

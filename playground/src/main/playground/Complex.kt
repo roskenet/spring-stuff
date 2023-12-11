230e958219b2f646628404a79fc74480c8196681
Felix Roske <felix@roskenet.de>
2023-12-11 08:59:28 +0100

    Implement complex arithmetics

--- playground/src/main/playground/Complex.kt
+++ playground/src/main/playground/Complex.kt
@@ -1,19 +1,19 @@
-data class Complex(val real: Double, val imaginary: Double) {
+data class Complex(val real: Double, val imag: Double) {
     operator fun plus(c: Complex) =
-        Complex(real + c.real, imaginary + c.imaginary)
+        Complex(real + c.real, imag + c.imag)
     operator fun minus(c: Complex) =
-        Complex(real - c.real, imaginary - c.imaginary)
+        Complex(real - c.real, imag - c.imag)
     operator fun times(c: Complex) =
-        Complex(real * c.real - imaginary * c.imaginary, real * c.imaginary + imaginary * c.real)
+        Complex(real * c.real - imag * c.imag, real * c.imag + imag * c.real)
     operator fun div(c: Complex): Complex {
-        val denominator = c.real * c.real + c.imaginary * c.imaginary
+        val denom = c.real * c.real + c.imag * c.imag
         return Complex(
-            (real * c.real + imaginary * c.imaginary) / denominator,
-            (imaginary * c.real - real * c.imaginary) / denominator
+            (real * c.real + imag * c.imag) / denom,
+            (imag * c.real - real * c.imag) / denom
         )
     }
 
-    override fun toString() = "$real + ${imaginary}i"
+    override fun toString() = "$real + ${imag}i"
 }
 
 fun main() {
